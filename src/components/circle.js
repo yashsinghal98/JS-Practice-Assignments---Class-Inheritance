cdf1d75523038cef439a9e3fd439fc072b7982bd
yashsinghal98 <68867021+yashsinghal98@users.noreply.github.com>
2020-10-24 15:39:16 +0530

    changes

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,12 @@
 // import using require
-
+import Shape from "./shape";
 // declare class
-
-
+class Circle extends Shape{
+  constructor(props)
+  {
+    super(props);
+    this.calculateArea=()=>{};
+  }
+}
+export default Circle;
 // export class using module.exports
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,13 @@
 // declare class
+class Shape{
+  constructor(props)
+  {
+    this.color=props;
+  }
+  drawShape(){
 
+  }
+  calculateArea(){}
+}
+export default Shape;
 // export class using module.exports
